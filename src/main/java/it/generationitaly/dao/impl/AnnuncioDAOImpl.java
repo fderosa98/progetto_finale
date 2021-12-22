9afc74c0cc8ef6d859ebb0fa1696729bde5ec9a4
Luca Boltri <Luca Boltri@DESKTOP-8QNT7JU.lan>
2021-12-22 10:36:08 +0100

    beccate sto fix

--- src/main/java/it/generationitaly/dao/impl/AnnuncioDAOImpl.java
+++ src/main/java/it/generationitaly/dao/impl/AnnuncioDAOImpl.java
@@ -11,6 +11,9 @@ import it.generationitaly.dao.AnnuncioDAO;
 import it.generationitaly.dao.DAOException;
 import it.generationitaly.dao.DBUtil;
 import it.generationitaly.model.Annuncio;
+import it.generationitaly.model.Automobile;
+import it.generationitaly.model.Carburante;
+import it.generationitaly.model.NumeroPorte;
 
 public class AnnuncioDAOImpl implements AnnuncioDAO {
 
@@ -43,35 +46,83 @@ public class AnnuncioDAOImpl implements AnnuncioDAO {
 
 	public List<Annuncio> findFiltered(Connection connection, String marca, String modello, double prezzo)
 			throws DAOException {
-		String sql = "SELECT * FROM automobile";
-		int count = 0;
+		String sql = "SELECT * FROM annuncio join automobile on annuncio.automobile_id = automobile.id";
+
 		if (marca != null || prezzo > 0) {
-			sql += "WHERE marca LIKE=?";
-			count++;
+			sql += " WHERE";
+			if (marca != null) {
+				sql += " marca LIKE ?";
+			}
 			if (modello != null) {
-				sql += "AND modello LIKE=?";
+				sql += " AND modello LIKE ?";
+			}
+			if (marca != null && prezzo > 0) {
+				sql += " AND prezzo<=?";
+			}
+			if (marca == null && prezzo > 0) {
+				sql += " prezzo<=?";
 			}
 		}
-			if (prezzo > 0) {
-				sql += "WHERE prezzo<=?";
-				count++;
-			}
 
-			System.out.println(sql);
-			PreparedStatement statement = null;
-			ResultSet resultSet = null;
-			try {
-
-				statement = connection.prepareStatement(sql);
+		System.out.println(sql);
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
+		try {
+			statement = connection.prepareStatement(sql);
+			if (marca != null) {
 				statement.setString(1, "%" + marca + "%");
-				statement.setString(2, "%" + modello + "%");
-				statement.setDouble(3, prezzo);
-				resultSet = statement.executeQuery();
-				while (resultSet.next()) {
-
-				}
-			} catch (SQLException e) {
 			}
+			if (modello != null) {
+				statement.setString(2, "%" + modello + "%");
+			}
+			if (marca == null && prezzo > 0) {
+				statement.setDouble(1, prezzo);
+			} else if (marca != null && modello == null && prezzo > 0) {
+				statement.setDouble(2, prezzo);
+			} else if (marca != null && modello != null && prezzo > 0) {
+				statement.setDouble(3, prezzo);
+			}
+			resultSet = statement.executeQuery();
+			while (resultSet.next()) {
+				Annuncio annuncio = new Annuncio();
+				annuncio.setId(resultSet.getInt(1));
+				annuncio.setTitolo(resultSet.getString(2));
+				annuncio.setDescrizione(resultSet.getString(3));
+
+				Automobile automobile = new Automobile();
+				automobile.setId(resultSet.getInt(5));
+				automobile.setMarca(resultSet.getString(8));
+				automobile.setModello(resultSet.getString(9));
+				automobile.setAnno(resultSet.getInt(10));
+				automobile.setPrezzo(resultSet.getDouble(11));
+				automobile.setKm(resultSet.getInt(12));
+				automobile.setCarburante(Carburante.valueOf(resultSet.getString(13)));
+				automobile.setNumeroPorte(NumeroPorte.valueOf(resultSet.getString(14)));
+			}
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+			throw new DAOException();
+		}
 		return null;
 	}
+
+//	private String createQuery(String marca, String modello, double prezzo) {
+//		String sql = "SELECT * FROM automobile";
+//		if (marca != null || prezzo > 0) {
+//			sql += " WHERE";
+//			if (marca != null) {
+//				sql += " marca LIKE ?";
+//			}
+//			if (modello != null) {
+//				sql += " AND modello LIKE ?";
+//			}
+//			if (marca != null && prezzo > 0) {
+//				sql += " AND prezzo<=?";
+//			}
+//			if (marca == null && prezzo > 0) {
+//				sql += " prezzo<=?";
+//			}
+//		}
+//		return sql;
+//	}
 }
