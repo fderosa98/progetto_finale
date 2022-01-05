69e17deb657f1819c2583b5e51c563aa23b337cb
paolo <paulp@NB-PAOLO.station>
2022-01-05 21:24:45 +0100

    cambiate import javax con jakarta

--- src/main/java/it/generationitaly/control/LoginServlet.java
+++ src/main/java/it/generationitaly/control/LoginServlet.java
@@ -6,11 +6,11 @@ import it.generationitaly.model.Utente;
 import it.generationitaly.service.ServiceException;
 import it.generationitaly.service.UtenteService;
 import it.generationitaly.service.impl.UtenteServiceImpl;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 /**
  * Servlet implementation class LoginServlet
