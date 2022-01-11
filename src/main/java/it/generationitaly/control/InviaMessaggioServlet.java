778f2440d18a61eef6d15690498340f40673b343
paolo <paulp@NB-PAOLO.station>
2022-01-11 12:54:36 +0100

    torna sulla stessa pagina con annuncio

--- src/main/java/it/generationitaly/control/InviaMessaggioServlet.java
+++ src/main/java/it/generationitaly/control/InviaMessaggioServlet.java
@@ -1,11 +1,15 @@
 package it.generationitaly.control;
 
 import java.io.IOException;
+import java.util.List;
 
+import it.generationitaly.model.Annuncio;
 import it.generationitaly.model.Messaggio;
 import it.generationitaly.model.Utente;
+import it.generationitaly.service.AnnuncioService;
 import it.generationitaly.service.ServiceException;
 import it.generationitaly.service.UtenteService;
+import it.generationitaly.service.impl.AnnuncioServiceImpl;
 import it.generationitaly.service.impl.UtenteServiceImpl;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -22,6 +26,7 @@ public class InviaMessaggioServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	UtenteService utenteService = new UtenteServiceImpl();
+	AnnuncioService annuncioService = new AnnuncioServiceImpl();
 
 	/**
 	 * Default constructor.
@@ -44,13 +49,16 @@ public class InviaMessaggioServlet extends HttpServlet {
 		}
 		
 		String username = (String) request.getSession().getAttribute("username");
-		// il destinatario é colui che ha pubblicato l'annuncio su cui siamo noi
+		int idAnnuncio = Integer.parseInt(request.getParameter("idAnnuncio"));
 		int idDestinatario = Integer.parseInt(request.getParameter("idDestinatario"));
 		String message = request.getParameter("message");
 		Utente mittente = null;
 		Utente destinatario = null;
 		Messaggio messaggio = new Messaggio();
+		List<Annuncio> annunci = null;
 		try {
+			Annuncio annuncio = annuncioService.findDettaglioById(idAnnuncio);
+			annunci = annuncioService.findAll();
 			mittente = utenteService.findByUsername(username);
 			destinatario = utenteService.findById(idDestinatario);
 			if(mittente.getId() == destinatario.getId()) {
@@ -62,7 +70,10 @@ public class InviaMessaggioServlet extends HttpServlet {
 				messaggio.setDestinatario(destinatario);
 				messaggio.setCorpoMessaggio(message);
 				utenteService.saveMessaggio(messaggio);
-				response.sendRedirect("listing-detail.jsp");
+				request.setAttribute("annuncio", annuncio);
+				request.setAttribute("annunci", annunci);
+				request.getRequestDispatcher("listing-detail.jsp?inviato").forward(request, response);
+				
 			}
 		} catch (ServiceException e) {
 			System.err.println(e.getMessage());
